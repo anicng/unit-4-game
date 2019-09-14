3cec8326f350938fd8e75e5b7b513944709605c3
Annie <annie.yiyunc@gmail.com>
2019-09-13 20:03:07 -0700

    pseudocode jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+
+// generate a targetNumber between 19-120
+
+
+
+
+
+// assign each img a value between 1-12
+.attr('',Math.floor(Math.random() * 10))+1)
+
+
+
+
+// When click on the image, 
+$(".img").on('click', function(){
+    // add it's assigned value to currentScore
+
+});
+
+
+// if currentScore = targetNumber win++
+
+
+// if currentScore > targetNumber losses++
+
+
+
+// start new game
