b5c5711a003cc781cbf7e7e1cb11a1d9f01312aa
Annie <annie.yiyunc@gmail.com>
2019-09-13 20:28:38 -0700

    Generated a random number between 19-120 and append to target-number id

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,31 @@
-
-// generate a targetNumber between 19-120
-
-
-
-
-
-// assign each img a value between 1-12
-.attr('',Math.floor(Math.random() * 10))+1)
+$(document).ready(function () {
+    // generate a targetNumber between 19-120
+    var targetNumber = Math.floor(Math.random() * 120) + 19;
+    console.log("targetNumber:" + targetNumber);
+    // display on target-number id
+    $("#target-number").append(targetNumber);
 
 
+    // assign each img a value between 1-12
+    // .attr('',Math.floor(Math.random() * 10)+1)
 
 
-// When click on the image, 
-$(".img").on('click', function(){
+
+
+    // When click on the image, 
+    // $(".img").on('click', function(){
     // add it's assigned value to currentScore
 
+    // });
+
+
+    // if currentScore = targetNumber win++
+
+
+    // if currentScore > targetNumber losses++
+
+
+
+    // start new game
+
 });
-
-
-// if currentScore = targetNumber win++
-
-
-// if currentScore > targetNumber losses++
-
-
-
-// start new game
